e1562f1a2894657d7a6b70e6207537a758c11a2e
jason <jasonedlewis@gmail.com>
2019-07-24 11:50:37 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,6 @@
 class Customer
   attr_reader :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
@@ -9,4 +9,30 @@ class Customer
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select{|review| review.author == self}
+  end
+
+  def num_reviews
+    reviews.count
+  end
+
+  def restaurants
+    reviews.map{|review| review.restaurant}.uniq
+  end
+
+  def add_review(restaurant,content, rating)
+    new_rewiew = Review.new(restaurant, self,content,rating)
+    new_rewiew
+  end
+
+  def self.find_by_first_name(name)
+    Customer.all.find{|customer| customer.first_name== name}
+end
+
 end
