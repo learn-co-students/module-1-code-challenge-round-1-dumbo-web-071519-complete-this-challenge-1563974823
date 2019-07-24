1f3419556f0b067c66755dd42f2fdb7bdd990e0d
Gulnoza Muminova <gmcharmy@gmail.com>
2019-07-24 10:37:46 -0400

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
+  @@all = []
+   attr_reader :author, :restaurant
+   attr_accessor :rating, :content
+
+  def initialize(restaurant, author, content, rating)
+    @restaurant = restaurant
+    @author = author
+    @rating = rating
+    @content = content
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
 end
 
