9476bd5a279d09e5438b9c43f3ffca0ef349522e
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-07-24 10:29:40 -0400

    Finished challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
+  @@all = []
+
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    #returns a list of reviews for this restaurant
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+   customer_list =  self.reviews.map{|review| review.customer}
+   customer_list.uniq
+  end
+
+  def average_star_rating
+    rating_list = self.reviews.map{|review| review.rating}
+    avg = (rating_list.inject(0){|sum, i| sum+i}).to_f/(rating_list.size).to_f
+    avg
+  end
+
+  def longest_review
+    content_list = self.reviews.map {|review| review.content}
+    sorted = content_list.sort_by{|content| content.size}
+    sorted.last
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|restaurant| restaurant.name == name}
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+c1 = Customer.new("Bob", "Smith")
+c2 = Customer.new("Bob", "Jones")
+c3 = Customer.new("Eric", "Donnel")
+c4 = Customer.new("John", "Doe")
+
+res1 = Restaurant.new("Chili's")
+res2 = Restaurant.new("Applebee's")
+res3 = Restaurant.new("Outback Steakhouse")
+
+rev1 = Review.new(c1, res1, 5, "I seriously love Chili's. Especially when they greet me. Welcome to Chili's!")
+rev2 = Review.new(c2, res1, 5, "CHILI'S. I NEEEEEEEEED IT")
+rev3 = Review.new(c1, res2, 2, "This place sucks. Chili's is much better")
+rev4 = Review.new(c3, res3, 1, "They don't say welcome to Chili's when I walked in so I immediately left. I wish I could give this 0 stars. ")
+rev5 = Review.new(c1, res1, 5, "WELCOME TO CHILI'S!!!!!!!!!!!")
+rev6 = Review.new(c4, res3, 0, "Yeah it's fine.")
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
