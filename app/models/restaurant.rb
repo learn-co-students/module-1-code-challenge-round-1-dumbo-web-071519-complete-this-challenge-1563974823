072dc04d63651e74ce88016247f97527b48b123f
Carlos Guzman <tomaffew@me.com>
2019-07-24 10:38:19 -0400

    blanked

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,40 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  #`Restaurant#customers`
+  # Returns a **unique** list of all customers who have reviewed a particular restaurant.
+  def customers
+
+  end
+
+  #`Restaurant#reviews`
+  #returns an array of all reviews for that restaurant
+  def reviews
+
+  end
+
+  #`Restaurant#average_star_rating`
+  #returns the average star rating for a restaurant based on its reviews
+  def average_star_rating
+
+  end
+
+  #`Restaurant#longest_review`
+  #returns the longest review content for a given restaurant
+  def longest_review
+
+  end
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review
-  
+
+
+    #`Review#customer`
+    #returns the customer object for that given review
+    #Once a review is created, I should not be able to change the author
+    
+
+
 end
 
