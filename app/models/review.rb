e5616b82bc833982f8de00a0bc4c94cd88e9f1f1
Lilaro <larochelle.lisa@gmail.com>
2019-07-24 10:38:48 -0400

    challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,46 @@
 class Review
-  
+    attr_reader :restaurant, :customer, :rating, :content
+
+    @@all = []
+
+    def initialize(restaurant, customer, rating, content)
+        @restaurant = restaurant 
+        @customer = customer
+        @rating = rating
+        @content = content
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def one_restaurants_reviews(rest_id)
+        self.all.select do |review|
+            review.restaurant == rest_id
+        end
+    end
+
+    def this_reviews_customer
+        self.customer
+    end
+
+    def this_reviews_restaurant
+        self.restaurant
+    end
+
+    def this_reviews_content
+        self.content
+    end
+      
+
+
+
+    # def customer
+    #     self.all.select do |review|
+    #         review.customer ==
+
+
+
 end
 
