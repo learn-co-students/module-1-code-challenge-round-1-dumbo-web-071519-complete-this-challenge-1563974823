b88085a5881a2372c3373d1016146c64ba07d676
Megan <MeganOliviaWendyAdams@gmail.com>
2019-07-24 10:38:02 -0400

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,54 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+def add_review(restaurant, content, rating)
+  Review.new(restaurant, self, content, rating)
+end 
+
+
+def num_reviews
+counter = Review.all.select {|rev| rev.customer == self}.count
+counter
+  end
+
+
+def reviews
+ Review.all.select {|rev| rev.customer == self} 
+  end
+
+
+
+def self.find_by_name(name)
+  # given a string of a **full name**, returns the **first customer** whose full name matches
+end
+
+def Customer.find_all_by_first_name(name)
+  # given a string of a first name, returns an **array** containing all customers with that first name
+end
+
+
+def self.all_names
+self.all
+  end
+
+
+
+
+
+  def self.all 
+    @@all 
+  end 
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
+
+    @@all = [] 
+
+    attr_accessor :restaurant, :customer, :rating, :content
+
+    def initialize(restaurant, customer, rating, content)
+        @restaurant = restaurant
+        @customer = customer
+        @rating = rating
+        @content = content
+        @@all << self
+    end
+
+
+
+
+    def self.all 
+        @@all 
+      end 
+
+
   
 end
 
