c5770d891641a1d229614415648dca051dcc6b18
vertreesa <Alessandra.Vertrees82@myhunter.cuny.edu>
2019-07-24 10:32:11 -0400

    done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
+    attr_accessor :restaurant, :customer, :content, :rating
+    @@all = []
+
+    def initialize(restaurant, customer, content, rating)
+        @restaurant = restaurant
+        @customer = customer
+        @content = content
+        @rating = rating.to_f
+        @@all << self
+    end
+    
+    # - `Review.all`
+    #   - returns all of the reviews
+    def self.all 
+        @@all
+    end
+
 end
 
